067af4a2fd86cb660a94fcb7b976d655296dbe1b
Tayanovsky Ilya <ilatayanovskij@gmail.com>
2026-06-16 20:01:34 +0300

    fix2

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 def copy_file(command: str) -> None:
     parts = command.split()
 
-    if len(parts) != 3:
+    if len(parts) < 3:
         return
-
     if parts[0] != "cp":
         return
-
-    src, dst = parts[1], parts[2]
-
-    if src == dst:
+    if parts[1] == parts[2]:
         return
 
     try:
-        with open(src, "r") as file, open(dst, "w") as new_file:
-            new_file.write(file.read())
+        with open(parts[1], "r") as file_read:
+            content = file_read.read()
+
+        with open(parts[2], "w") as file_write:
+            file_write.write(content)
 
     except FileNotFoundError:
         return
